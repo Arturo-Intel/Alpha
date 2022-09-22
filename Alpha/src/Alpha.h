108125dfe9befa44f68dc84895c72d7096261198
Arturo M <arturorules@gmail.com>
2022-09-21 19:02:28 -0500

    Adding Log system

--- Alpha/src/Alpha.h
+++ Alpha/src/Alpha.h
@@ -2,6 +2,8 @@
 
 // For use by Alpha Apps.
 #include "GLCore/Application.h"
+#include "GLCore/Log.h"
+
 
 // Entry point
 #include "GLCore/EntryPoint.h"
--- Alpha/src/GLCore/EntryPoint.h
+++ Alpha/src/GLCore/EntryPoint.h
@@ -6,6 +6,11 @@ extern Alpha::Application* Alpha::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Alpha::Log::Init();
+	LOG_CORE_WARN("Initialized Log!");
+	int a = 5;
+	LOG_INFO("Hello {0}" ,a);
+
 	auto app = Alpha::CreateApplication();
 	app->Run();
 	delete app;
