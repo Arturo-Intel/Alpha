8906075e00a2076cde2ba3c482fe3d21d2fb15fa
Arturo M <arturorules@gmail.com>
2022-09-21 20:18:45 -0500

    Added Build System using premake https://github.com/premake/premake-core Added generateProj batch - for project generation Reorg Sandbox cpp files

---
+++ premake5.lua
@@ -0,0 +1,109 @@
+workspace "Alpha"
+	architecture "x64"
+	
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Alpha"
+	location "Alpha"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines 
+		{
+			"ALPHA_PLATFORM_WINDOWS",
+			"ALPHA_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "ALPHA_DEBUG"
+		symbols "On"
+	
+
+	filter "configurations:Release"
+		defines "ALPHA_RELEASE"
+		optimize "On"
+	
+
+	filter "configurations:Dist"
+		defines "ALPHA_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"Alpha/vendor/spdlog/include",
+		"Alpha/src"
+	}
+
+	links
+	{
+		"Alpha"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines 
+		{
+			"ALPHA_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "ALPHA_DEBUG"
+		symbols "On"
+	
+
+	filter "configurations:Release"
+		defines "ALPHA_RELEASE"
+		optimize "On"
+	
+
+	filter "configurations:Dist"
+		defines "ALPHA_DIST"
+		optimize "On"
