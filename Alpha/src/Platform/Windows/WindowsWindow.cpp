71f6c83f5989ed771aa19fb6bcebe9e75b42fe88
Arturo M <arturorules@gmail.com>
2022-09-23 12:31:13 -0500

    Integrate Glad loader generator

--- Alpha/src/Platform/Windows/WindowsWindow.cpp
+++ Alpha/src/Platform/Windows/WindowsWindow.cpp
@@ -5,6 +5,8 @@
 #include "EngineCore/Events/KeyEvent.h"
 #include "EngineCore/Events/MouseEvent.h"
 
+#include <glad/glad.h>
+
 namespace Alpha {
 	
 	static bool s_GLFWInitialized = false;
@@ -46,6 +48,10 @@ namespace Alpha {
 
 		m_Window = glfwCreateWindow((int)props.Width, (int)props.Height, m_Data.Title.c_str(), nullptr, nullptr);
 		glfwMakeContextCurrent(m_Window);
+
+		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+		ALPHA_CORE_ASSERT(status, "Failed to initialize Glad! ");
+
 		glfwSetWindowUserPointer(m_Window, &m_Data);
 		SetVSync(true);
 
--- premake5.lua
+++ premake5.lua
@@ -13,8 +13,10 @@ outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 -- Include directories relative to root folder (solution directory)
 IncludeDir = {}
 IncludeDir["GLFW"] = "Alpha/vendor/GLFW/Include"
+IncludeDir["Glad"] = "Alpha/vendor/Glad/Include"
 
 include "Alpha/vendor/GLFW"
+include "Alpha/vendor/GLAD"
 
 project "Alpha"
 	location "Alpha"
@@ -37,12 +39,14 @@ project "Alpha"
 	{
 		"%{prj.name}/vendor/spdlog/include",
 		"Alpha/src",
-		"%{IncludeDir.GLFW}"
+		"%{IncludeDir.GLFW}",
+		"%{IncludeDir.Glad}"
 	}
 
 	links
 	{
 			"GLFW",
+			"Glad",
 			"opengl32.lib"
 	}
 
@@ -54,7 +58,8 @@ project "Alpha"
 		defines 
 		{
 			"ALPHA_PLATFORM_WINDOWS",
-			"ALPHA_BUILD_DLL"
+			"ALPHA_BUILD_DLL",
+			"GLFW_INCLUDE_NONE"
 		}
 
 		postbuildcommands
